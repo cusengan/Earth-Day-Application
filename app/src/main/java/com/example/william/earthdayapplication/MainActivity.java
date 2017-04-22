a4d0814596b388c7169cb021ba002ae4947ed13b
cusengan <lordvinh1@gmail.com>
2017-04-22 02:00:02 -0500

    Added functionality to the button item

--- app/src/main/java/com/example/william/earthdayapplication/MainActivity.java
+++ app/src/main/java/com/example/william/earthdayapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.william.earthdayapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +25,25 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemClicked = item.getItemId();
+
+        if(itemClicked == R.id.action_setting){
+            Toast.makeText(MainActivity.this,"You pushed the button!", Toast.LENGTH_LONG).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
